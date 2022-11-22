34b7d978e51191da2c683f36405373e258eee7ca
Liu-Steve <steve_liu@whu.edu.cn>
2022-11-23 01:43:27 +0800

    Create WebSocketService.java

---
+++ Server/server/src/main/java/com/carmanager/server/Service/impl/WebSocketService.java
@@ -0,0 +1,13 @@
+package com.carmanager.server.Service.impl;
+
+import com.carmanager.server.Entity.Point;
+import com.carmanager.server.Service.IWebSocketService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebSocketService implements IWebSocketService {
+    @Override
+    public void update(Point point) {
+
+    }
+}
