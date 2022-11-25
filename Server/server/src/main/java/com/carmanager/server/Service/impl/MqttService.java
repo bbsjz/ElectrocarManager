f652c937dd0488b3af9a9927b24c8989aeb2a0db
Liu-Steve <steve_liu@whu.edu.cn>
2022-11-26 00:08:13 +0800

    Mqtt successfully received msg

--- Server/server/src/main/java/com/carmanager/server/Service/impl/MqttService.java
+++ Server/server/src/main/java/com/carmanager/server/Service/impl/MqttService.java
@@ -2,8 +2,23 @@ package com.carmanager.server.Service.impl;
 
 import com.carmanager.server.Entity.Command;
 import com.carmanager.server.Service.IMqttService;
+import com.carmanager.server.Utils.MqttUtils;
+import com.carmanager.server.config.MqttConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+@Service
 public class MqttService implements IMqttService {
+
+    public MqttService() {
+        MqttUtils.subscribe(MqttConfig.TOPIC_SENSOR, (s, message) -> {
+            System.out.println(new String(message.getPayload(), StandardCharsets.UTF_8));
+        });
+    }
+
     @Override
     public void controlCar(Command command) {
 
---
+++ Server/server/src/main/java/com/carmanager/server/config/MqttConfig.java
@@ -0,0 +1,20 @@
+package com.carmanager.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqttConfig {
+
+    public static final String MQTT_SERVER_ADDRESS = "tcp://10.132.0.233:1883"; // broker-cn.emqx.io
+
+    public static final String MQTT_PUBLISHER_ID = "spring-server";
+
+    public static final String TOPIC_SENSOR = "car-manager/sensor";
+
+    public static final String TOPIC_LOCK = "car-manager/lock";
+
+    public static final int QOS = 0;
+
+    public static final boolean RETAINED = false;
+
+}
