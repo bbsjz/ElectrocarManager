f9fdd318775d24c9a58ff3901b45c0ec96ccaef1
Liu-Steve <steve_liu@whu.edu.cn>
2022-11-28 00:51:49 +0800

    try publish

--- Server/server/src/main/java/com/carmanager/server/ServerApplication.java
+++ Server/server/src/main/java/com/carmanager/server/ServerApplication.java
@@ -3,6 +3,8 @@ package com.carmanager.server;
 import com.carmanager.server.webSocket.WebSocketServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,11 +12,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @EnableWebMvc
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 		WebSocketServer.init();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 
 }
