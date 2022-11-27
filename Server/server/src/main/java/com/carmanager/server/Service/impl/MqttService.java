94fee7d597b22ffe3519a89985dd30df2c355bf0
Liu-Steve <steve_liu@whu.edu.cn>
2022-11-27 21:06:57 +0800

    Update MqttService.java

--- Server/server/src/main/java/com/carmanager/server/Service/impl/MqttService.java
+++ Server/server/src/main/java/com/carmanager/server/Service/impl/MqttService.java
@@ -4,10 +4,12 @@ import com.carmanager.server.Entity.Command;
 import com.carmanager.server.Service.IMqttService;
 import com.carmanager.server.Utils.MqttUtils;
 import com.carmanager.server.config.MqttConfig;
+import com.carmanager.server.mqtt.MqttFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
 
 @Service
 public class MqttService implements IMqttService {
@@ -15,15 +17,22 @@ public class MqttService implements IMqttService {
     private final MqttUtils utils;
     private final MqttConfig config;
 
+    private static final Logger logger = Logger.getLogger(String.valueOf(MqttFactory.class));
+
     public MqttService(MqttUtils utils, MqttConfig config) {
         this.utils = utils;
         this.config = config;
     }
 
+    /**
+     * 订阅来自硬件的消息
+     */
     @PostConstruct
     public void init() {
         utils.subscribe(config.TOPIC_SENSOR, (s, message) -> {
-            System.out.println(new String(message.getPayload(), StandardCharsets.UTF_8));
+            logger.info("Receive message from topic: " +
+                    config.TOPIC_SENSOR +
+                    (new String(message.getPayload(), StandardCharsets.UTF_8)));
         });
     }
 
